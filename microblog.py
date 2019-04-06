f4e5a883e29ebd1dab751123eeda4df27823eea3
BorisykVova <doterua66@gmail.com>
2019-04-06 22:37:47 +0300

    add git

---
+++ microblog.py
@@ -0,0 +1,11 @@
+from app import current_app, db, cli
+from app.models import User, Post
+
+
+app = current_app()
+cli.register(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
